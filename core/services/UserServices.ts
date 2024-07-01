596335a63e1c2c0affa4393f7a73f539ba903256
pabloaugmat <pabloaugmat@gmail.com>
2024-07-01 20:41:10 -0300

    correcao do merge

--- core/services/UserServices.ts
+++ core/services/UserServices.ts
@@ -1,11 +1,7 @@
 import { useApi } from "../hooks/useApi";
 import { Info } from "../models/User";
 
-<<<<<<< HEAD
-const url = "https://042d5ba28237544aeee54f79addd0235.loophole.site";
-=======
 const url = "http://3.227.175.217:8082";
->>>>>>> origin/master
 
 export function UserServices() {
   const api = useApi();
